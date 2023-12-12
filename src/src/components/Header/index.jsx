fbea5ac5260d935d90e5c9a941acfa1a0dcb75bf
VictorGM01 <victormarques8801@gmail.com>
2023-12-12 17:20:23 -0300

    feat: estrutura inicial do header

--- src/src/components/Header/index.jsx
+++ src/src/components/Header/index.jsx
@@ -1,9 +1,17 @@
 import styles from './styles.module.css';
+import logo from '../../assets/logo.png';
+import menu from '../../assets/menu.png';
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
-			<h1 className={styles.titulo}>Header</h1>
+			<div className={styles.container}>
+				<img src={logo} alt="Logo da Liga" className={styles.logo}/>
+				<div className={styles.container__icones}>
+					<button className={styles.button}>Login</button>
+					<img src={menu} alt="Ícone de menu" className={styles.menu}/>
+				</div>
+			</div>
 		</header>
 	);
 }
